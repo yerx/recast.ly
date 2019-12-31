be411f8281db85074ece1636ca245e5be61fa3a9
yerx <yer.x.xiong@gmail.com>
2019-12-30 20:17:20 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 // TODO: Render the `App` component to the DOM
-import App from './components/App.js';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+// import App from './components/App.js';
+import App from './App.js';
 
+ReactDOM.render(<App />, document.getElementById('root'));
